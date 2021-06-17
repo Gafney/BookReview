6210671c36219f165d7c239051b78d2b46f169d9
Shannonlee Gafney <shannongafney@gmail.com>
2021-06-17 17:04:14 +0100

    First Upload

--- BookReview/Shared/BookReviews.cs
+++ BookReview/Shared/BookReviews.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace BookReview.Shared
 {
     public class BookReviews
     {
-        public BookReviews()
-        {
-        }
+        public string BookTitle { get; set; }
+
+        public string ReviewDiscription { get; set; }
+
+        public Stairs StairRating { get; set; }
+
+        public int Upvotes { get; set; }
+
+        public int Downvotes { get; set; }
+
+
+    }
+
+    public enum Stairs
+    {
+        stair_1,
+        stair_2,
+        stair_3,
+        stair_4,
+        stair_5
+
     }
 }
